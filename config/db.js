e6eedbbc28608efef66ec945ebe1de8c6076e22f
Khushagra Sharma <khushagrasharma002@gmail.com>
2026-01-03 16:29:20 +0530

    DB config isupdated

--- config/db.js
+++ config/db.js
@@ -4,15 +4,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Create connection pool for better performance
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "khushagra007",
+//   database: process.env.DB_NAME || "psychoish_db",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "khushagra007",
   database: process.env.DB_NAME || "psychoish_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
+
+
+
 
 // Get promise-based connection
 const db = pool.promise();
